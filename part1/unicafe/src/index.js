09d1a9ab372f593329942319dfba0a1b8e050a3d
SoulAdor <niedens.a@gmail.com>
2019-10-03 18:30:37 +0400

    Finished Exercises

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,43 +7,83 @@ const Header = ({name}) => (
     </h1>
 )
 
-const Statistic = ({name, value}) => (
-    <div>
-        {name} {value}
-    </div>
+const Button = ({name, onClick}) => (
+    <button onClick={onClick}>{name}</button>
 )
 
-const Statistics = (props) => (
-    <div>
-       
-    </div>
+const Statistic = ({name, value}) => (
+    <tr>
+        <td>{name}</td>
+        <td>{value}</td>
+    </tr>
 )
+
+const StatisticsInfo = ({statistic}) => {
+    if (statistic.total !== 0) return (
+        <table> 
+            <tbody>
+                <Statistic name={'good'}        value={statistic.good} />
+                <Statistic name={'neutral'}     value={statistic.neutral} />
+                <Statistic name={'bad'}         value={statistic.bad} />
+                <Statistic name={'total'}       value={statistic.total} />
+                <Statistic name={'average'}     value={statistic.average} />
+                <Statistic name={'positive'}    value={[statistic.positive, ' %']} />
+            </tbody>
+        </table>
+    )
+    else return (
+        <div>
+            No feedback given
+        </div>
+    )
+}
+
+const Statistics = ({statistic}) => {
+    return (
+        <div>
+            <Header name={'statistics'} />
+            <StatisticsInfo statistic={statistic} />
+        </div>
+    )
+}
 
 const App = () => {
-    // save clicks of each button to own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [statistic, setStatistic] = useState({
+        good: 0, neutral: 0, bad: 0, total: 0, average: 0, positive: 0
+    })
 
-    const setToValue = (setter, value) => () => {
-        setter(value + 1)
+    const updateStatistic = (good, neutral, bad) => {
+        const total = good + neutral + bad
+        const newStatistic = { 
+            good: good, 
+            neutral: neutral,
+            bad: bad,
+            total: total,
+            average: (good - bad) / total,
+            positive: 100 * good / total
+        }
+        setStatistic (newStatistic)
+    }
+
+    const onClickGood = () => {
+        updateStatistic (statistic.good + 1, statistic.neutral, statistic.bad)
+    } 
+
+    const onClickNeutral = () => {
+        updateStatistic (statistic.good, statistic.neutral + 1, statistic.bad)
+    }
+
+    const onClickBad = () => {
+        updateStatistic (statistic.good, statistic.neutral, statistic.bad + 1)
     }
 
     return (
     <div>
-
         <Header name={'give feedback'} />
-
-        <button onClick={setToValue(setGood, good)}>good</button>
-        <button onClick={setToValue(setNeutral, neutral)}>neutral</button>
-        <button onClick={setToValue(setBad, bad)}>bad</button>
-
-        <Header name={'statistics'} />
-
-        <Statistic name={'good'} value={good} />
-        <Statistic name={'neutral'} value={neutral} />
-        <Statistic name={'bad'} value={bad} />
-
+        <Button onClick={onClickGood}       name={'good'} />
+        <Button onClick={onClickNeutral}    name={'neutral'} />
+        <Button onClick={onClickBad}        name={'bad'} />
+        <Statistics statistic={statistic} />
     </div>
     )
 }
