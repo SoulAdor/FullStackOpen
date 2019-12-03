f17fe5c12cac9415a8690052fcee83d50c6f31a6
SoulAdor <niedens.a@gmail.com>
2019-12-03 20:16:14 +0400

    Updated for part 5

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -13,23 +13,25 @@ blogsRouter.post('/', async (request, response, next) => {
   try {
     // Check token
     const token = request.token
-    if (!token) return  response.status(401).json({ error: 'token missing' })
+    if (!token) return response.status(401).json({ error: 'token missing' })
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!decodedToken.id) return  response.status(401).json({ error: 'invalid token' })
 
     // Find user with given id
-    const user = await User.findById(decodedToken.id) //User.findOne({ 'username': "SoulAdor" }).exec()
+    // const user = User.findOne({ 'username': "SoulAdor" }).exec()
+    const user = await User.findById(decodedToken.id)
 
     // Save blog
     const blog = new Blog(request.body)
     blog.user = user._id
     const savedBlog = await blog.save()
-
+    
     // Update user
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
 
-    response.status(201).json(savedBlog)
+    const populatedBlog = await Blog.findOne(savedBlog).populate('user', { username: 1, name: 1, id: 1 })
+    response.status(201).json(populatedBlog)
   } catch(exception) {
     next (exception)
   }
@@ -37,18 +39,8 @@ blogsRouter.post('/', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    // Check token
-    const token = request.token
-    if (!token) return  response.status(401).json({ error: 'token missing' })
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!decodedToken.id) return  response.status(401).json({ error: 'invalid token' })
-    const userid = decodedToken.id
-
     const blog = await Blog.findById(request.params.id)
-    if (!blog) return  response.status(401).json({ error: 'Blog not found' })
-
-    if ( blog.user.toString() !== userid.toString() ) 
-      return response.status(401).json({ error: 'wrong user' })
+    if (!blog) return  response.status(404).json({ error: 'Blog not found' })
 
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
@@ -59,15 +51,17 @@ blogsRouter.delete('/:id', async (request, response, next) => {
 
 // Changes person of database
 blogsRouter.put('/:id', async (request, response, next) => {
-  try { 
+  try {
     const body = request.body
+    if ( !body.user ) return response.status(400).json({ error: 'no user in body' }) 
     const blog = {
+      user: body.user.id,
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes
     }
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true }).populate('user', { username: 1, name: 1, id: 1 })
     response.json(updatedBlog.toJSON())
   } catch(exception) {
     next (exception)
